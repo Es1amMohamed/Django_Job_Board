05749dbbe5ce27f4d138f7516296c8e143852acc
Es1amMohamed <EslamMohamedMetwally1@gmail.com>
2022-04-16 22:18:14 +0200

    'create_views_and_url'

--- Job/models.py
+++ Job/models.py
@@ -9,22 +9,27 @@ Job_Type = (
     ('Part Time' , 'Part Time'),
 )
 
-Category = (
-    ('Frontend Developer','Frontend Developer'),
-    ('Backend Developer','Backend Developer'),
-)
+
 
 class Job(models.Model):
     #owner 
     title = models.CharField(max_length= 40)
     description = models.TextField(max_length= 500)
     job_Responsibility = models.TextField(max_length= 1000)
-    category = models.CharField(max_length = 50 ,choices=Category)
     Published_at = models.DateTimeField(auto_now=True)
     type= models.CharField(max_length = 20 , choices = Job_Type)
     vacancy = models.IntegerField(default= 1 )
     salary = models.IntegerField(default= 0)
     experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category', null=True ,blank=True,on_delete= models.CASCADE)
     
     def __str__(self):
         return self.title
+    
+    
+class Category(models.Model):
+    name = models.CharField(max_length= 30)
+    
+    def __str__(self) :
+        return self.name
+    
---
+++ Job/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path , include
+from . import views
+
+
+urlpatterns = [
+  
+    path('', views.job_list,),
+    #path('',views.job_details,)
+]

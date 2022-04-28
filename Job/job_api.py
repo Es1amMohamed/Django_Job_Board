2c94ca7bb37c72c60d0aec75b5d6f6dc9d7ea374
Es1amMohamed <EslamMohamedMetwally1@gmail.com>
2022-04-28 21:05:41 +0200

    ready_to_upload

--- Job/job_api.py
+++ Job/job_api.py
@@ -1,10 +1,14 @@
 from .models import Job , Apply
+from account.models import *
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 class Job_Jason(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Job
-        fields = ['id','title','Published_at']
+        
+        fields = ['title']
+        
         
 class Apply_jason(serializers.HyperlinkedModelSerializer):
     class Meta:
--- Job/urls.py
+++ Job/urls.py
@@ -5,7 +5,6 @@ from .views import *
 
 app_name = 'Job'
 urlpatterns = [
-  
     path('', views.job_list,name = 'job_list'),
     path('add',views.add_job, name= 'add_job'),
     path('<str:slug>',views.job_details,name= 'job_detail')
